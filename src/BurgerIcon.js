bbc9c4fad4a2c5faa82d762c56a7047ea2e490d3
Imogen <imogen.wentworth@gmail.com>
2015-06-17 17:42:08 -0700

    Move icon components to separate files

---
+++ src/BurgerIcon.js
@@ -0,0 +1,58 @@
+var React = require('react');
+var appendVendorPrefix = require('react-kit/appendVendorPrefix');
+
+var BurgerIcon = React.createClass({
+
+  getLineStyle(index) {
+    return appendVendorPrefix({
+      position: 'fixed',
+      height: 6,
+      width: 36,
+      left: 36,
+      top: 36 + 12 * index,
+      zIndex: 1,
+      opacity: this.state.hover ? 0.6 : 1
+    });
+  },
+
+  handleHover() {
+    this.setState({ hover: !this.state.hover });
+  },
+
+  getInitialState() {
+    return { hover: false };
+  },
+
+  render() {
+    var buttonStyle = appendVendorPrefix({
+      position: 'fixed',
+      zIndex: 1,
+      margin: 24,
+      padding: 0,
+      width: 60,
+      height: 54,
+      border: 'none',
+      textIndent: 60,
+      fontSize: 24,
+      color: 'transparent',
+      background: 'transparent',
+      outline: 'none'
+    });
+
+    return (
+      <div>
+        <span className="bm-burger-icon" style={ this.getLineStyle(0) }></span>
+        <span className="bm-burger-icon" style={ this.getLineStyle(1) }></span>
+        <span className="bm-burger-icon" style={ this.getLineStyle(2) }></span>
+        <button onClick={ this.props.onClick }
+          onMouseEnter={ this.handleHover }
+          onMouseLeave={ this.handleHover }
+          style={ buttonStyle }>
+          Open Menu
+        </button>
+      </div>
+    );
+  }
+});
+
+export default BurgerIcon;
---
+++ src/CrossIcon.js
@@ -0,0 +1,47 @@
+var React = require('react');
+var appendVendorPrefix = require('react-kit/appendVendorPrefix');
+
+var CrossIcon = React.createClass({
+
+  getCrossStyle(type) {
+    return appendVendorPrefix({
+      position: 'absolute',
+      width: 3,
+      height: 14,
+      top: 14,
+      right: 18,
+      cursor: 'pointer',
+      transform: type === 'before' ? 'rotate(45deg)' : 'rotate(-45deg)',
+      zIndex: 1
+    });
+  },
+
+  render() {
+    var buttonStyle = appendVendorPrefix({
+      width: 14,
+      height: 14,
+      position: 'absolute',
+      right: 13,
+      top: 14,
+      padding: 0,
+      overflow: 'hidden',
+      textIndent: 14,
+      fontSize: 14,
+      border: 'none',
+      background: 'transparent',
+      color: 'transparent',
+      outline: 'none',
+      zIndex: 1
+    });
+
+    return (
+      <div>
+        <span className="bm-cross" style={ this.getCrossStyle('before') }></span>
+        <span className="bm-cross" style={ this.getCrossStyle('after') }></span>
+        <button onClick={ this.props.onClick } style={ buttonStyle }>Close Menu</button>
+      </div>
+    );
+  }
+});
+
+export default CrossIcon;
