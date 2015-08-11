4d8bd9b66e2c9186141c212d6d90aa793fe0e7ab
Imogen <imogen.wentworth@gmail.com>
2015-08-11 11:50:47 -0700

    Switch to eslint

--- src/menuFactory.js
+++ src/menuFactory.js
@@ -1,18 +1,17 @@
-var React = require('react');
-var Snap = require('snapsvg');
-var appendVendorPrefix = require('react-kit/appendVendorPrefix');
-var BurgerIcon = require('./BurgerIcon');
-var CrossIcon = require('./CrossIcon');
+import React from 'react';
+import snap from 'snapsvg';
+import BurgerIcon from './BurgerIcon';
+import CrossIcon from './CrossIcon';
 
 export default (styles) => {
 
   return React.createClass({
 
     propTypes: {
-      items: React.PropTypes.array,
+      children: React.PropTypes.array,
       id: React.PropTypes.string,
-      pageWrapId: React.PropTypes.string,
-      outerContainerId: React.PropTypes.string
+      outerContainerId: React.PropTypes.string,
+      pageWrapId: React.PropTypes.string
     },
 
     toggleMenu() {
@@ -48,19 +47,17 @@ export default (styles) => {
     // This is necessary for correct page interaction with some of the menus.
     // Throws and returns if the required external elements don't exist,
     // which means any external page animations won't be applied.
-    handleExternalWrapper(id, styles, set) {
-      var wrapper, wrapperStyles, prop;
-
-      wrapper = document.getElementById(id);
+    handleExternalWrapper(id, wrapperStyles, set) {
+      let wrapper = document.getElementById(id);
 
       if (!wrapper) {
         console.error("Element with ID '" + id + "' not found");
         return;
       }
 
-      wrapperStyles = styles(this.state.isOpen);
+      wrapperStyles = wrapperStyles(this.state.isOpen);
 
-      for (prop in wrapperStyles) {
+      for (let prop in wrapperStyles) {
         if (wrapperStyles.hasOwnProperty(prop)) {
           wrapper.style[prop] = set ? wrapperStyles[prop] : '';
         }
@@ -75,18 +72,21 @@ export default (styles) => {
 
     getDefaultProps() {
       return {
-        items: [],
         id: '',
-        pageWrapId: '',
-        outerContainerId: ''
-      }
+        outerContainerId: '',
+        pageWrapId: ''
+      };
     },
 
     getInitialState() {
-      return { isOpen: false }
+      return { isOpen: false };
     },
 
     componentWillMount() {
+      if (!styles || !Object.keys(styles).length) {
+        throw new Error('No styles supplied');
+      }
+
       // Warn if the selected menu requires external wrapper elements
       // but none were supplied.
       if (styles.pageWrap && !this.props.pageWrapId) {
@@ -111,11 +111,9 @@ export default (styles) => {
     },
 
     componentDidUpdate() {
-      var s, path;
-
       if (styles.svg) {
-        s = Snap('.bm-morph-shape');
-        path = s.select('path');
+        let s = snap('.bm-morph-shape');
+        let path = s.select('path');
 
         if (this.state.isOpen) {
           // Animate SVG path.
@@ -124,23 +122,35 @@ export default (styles) => {
           // Reset path (timeout ensures animation happens off screen).
           setTimeout(() => {
             path.attr('d', styles.svg.pathInitial);
-          }, 300)
+          }, 300);
         }
       }
     },
 
     render() {
-      var items, svg, closeButtonStyles;
+      let items, svg;
 
       // Add animation styles to user-defined menu items.
-      items = this.props.children.map((item, index) => {
-        var extraProps = {
-          key: index,
-          style: styles.item(this.state.isOpen, index + 1)
-        };
+      if (this.props.children) {
+        let extraProps;
 
-        return React.cloneElement(item, extraProps);
-      });
+        if (this.props.children.length) {
+          items = this.props.children.map((item, index) => {
+            extraProps = {
+              key: index,
+              style: styles.item(this.state.isOpen, index + 1)
+            };
+
+            return React.cloneElement(item, extraProps);
+          });
+        } else {
+          extraProps = {
+            style: styles.item(this.state.isOpen, 1)
+          };
+
+          items = React.cloneElement(this.props.children, extraProps);
+        }
+      }
 
       // Add a morph shape for animations that use SVG.
       if (styles.svg) {
@@ -172,4 +182,4 @@ export default (styles) => {
       );
     }
   });
-}
+};
