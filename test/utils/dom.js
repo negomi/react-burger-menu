facb41abd404fadfbfe94acca02f035b6310aa22
Imogen <imogen.wentworth@gmail.com>
2015-08-11 11:48:48 -0700

    Fix syntax

--- test/utils/dom.js
+++ test/utils/dom.js
@@ -1,11 +1,21 @@
-function dom() {
-  var jsdom = require('jsdom');
+import jsdom from 'jsdom';
 
+// From mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal(window) {
+  for (let key in window) {
+    if (!window.hasOwnProperty(key)) continue;
+    if (key in global) continue;
+
+    global[key] = window[key];
+  }
+}
+
+function dom() {
   // Set up the simplest document possible.
-  var doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+  let doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 
   // Get the window object out of the document.
-  var win = doc.defaultView;
+  let win = doc.defaultView;
 
   // Set globals for Mocha that make access to document and window feel
   // natural in the test environment.
@@ -15,16 +25,6 @@ function dom() {
   // Take all properties of the window object and also attach it to the
   // Mocha global object.
   propagateToGlobal(win);
-
-  // From mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
-  function propagateToGlobal(window) {
-    for (let key in window) {
-      if (!window.hasOwnProperty(key)) continue;
-      if (key in global) continue;
-
-      global[key] = window[key];
-    }
-  }
-};
+}
 
 dom();
