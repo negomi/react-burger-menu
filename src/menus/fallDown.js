48eef889f6b0c98c78da6273d4ad79616a8e6ee3
Imogen <imogen.wentworth@gmail.com>
2015-08-19 16:03:33 -0700

    Add fallDown tests

--- src/menus/fallDown.js
+++ src/menus/fallDown.js
@@ -1,59 +1,66 @@
-var menuFactory = require('../menuFactory');
-var appendVendorPrefix = require('react-kit/appendVendorPrefix');
+'use strict';
 
-var styles = {
+import menuFactory from '../menuFactory';
+
+let styles = {
 
   menuWrap(isOpen) {
-    return appendVendorPrefix({
+    return {
       position: 'fixed',
       zIndex: 2,
       width: 300,
       height: '100%',
+      WebkitTransform: isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(0, -100%, 0)',
       transform: isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(0, -100%, 0)',
+      WebkitTransition: 'all 0.5s ease-in-out',
       transition: 'all 0.5s ease-in-out'
-    });
+    };
   },
 
   menu() {
-    return appendVendorPrefix({
+    return {
       height: '100%'
-    });
+    };
   },
 
   item() {
-    return appendVendorPrefix({
+    return {
       display: 'block',
       outline: 'none'
-    });
+    };
   },
 
   overlay(isOpen) {
-    return appendVendorPrefix({
+    return {
       position: 'fixed',
       width: '100%',
       height: '100%',
       background: 'rgba(0, 0, 0, 0.3)',
       opacity: isOpen ? 1 : 0,
+      WebkitTransform: isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(-100%, 0, 0)',
       transform: isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      WebkitTransition: isOpen ? 'opacity 0.5s' : 'opacity 0.5s, transform 0.1s 0.5s',
       transition: isOpen ? 'opacity 0.5s' : 'opacity 0.5s, transform 0.1s 0.5s',
       zIndex: 1
-    });
+    };
   },
 
   pageWrap(isOpen) {
-    return appendVendorPrefix({
+    return {
+      WebkitTransform: isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(300px, 0, 0)',
       transform: isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(300px, 0, 0)',
+      WebkitTransition: 'all 0.5s',
       transition: 'all 0.5s'
-    });
+    };
   },
 
   outerContainer(isOpen) {
-    return appendVendorPrefix({
+    return {
       perspective: '1500px',
       perspectiveOrigin: '0% 50%',
       overflow: isOpen ? '' : 'hidden'
-    });
-  },
+    };
+  }
 };
 
 export default menuFactory(styles);
