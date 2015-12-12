ff7480ff433d31c973896aa0bc8f97c7634aaf7f
Imogen <imogen.wentworth@gmail.com>
2015-12-12 14:51:41 -0800

    Fix Firefox opacity bug

--- src/menus/bubble.js
+++ src/menus/bubble.js
@@ -23,7 +23,7 @@ const styles = {
         pos++;
       };
 
-      nextStep(pos);
+      nextStep();
     }
   },
 
@@ -50,7 +50,7 @@ const styles = {
     return {
       position: 'fixed',
       transform: isOpen ? 'translate3d(0, 0, 0)' : right ? `translate3d(${width}px, 0, 0)` : `translate3d(-${width}px, 0, 0)`,
-      transition: isOpen ? 'opacity 0.3s 0.4s cubic-bezier(.17, .67, .1, 1.27), transform 0.3s 0.4s cubic-bezier(.17, .67, .1, 1.27)' : 'opacity 0s 0.3s cubic-bezier(.17, .67, .1, 1.27), transform 0s 0.3s cubic-bezier(.17, .67, .1, 1.27)',
+      transition: isOpen ? 'opacity 0.1s 0.4s cubic-bezier(.17, .67, .1, 1.27), transform 0.1s 0.4s cubic-bezier(.17, .67, .1, 1.27)' : 'opacity 0s 0.3s cubic-bezier(.17, .67, .1, 1.27), transform 0s 0.3s cubic-bezier(.17, .67, .1, 1.27)',
       opacity: isOpen ? 1 : 0
     };
   },
