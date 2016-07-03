060cc3d58bc3f93ecc76284e8e3861e135894ad2
Imogen <imogen.wentworth@gmail.com>
2016-07-02 21:03:55 -0700

    Remove unsupported xmlns attr from <svg>

--- src/menuFactory.js
+++ src/menuFactory.js
@@ -188,7 +188,7 @@ export default (styles) => {
           <div id={this.props.id} className={"bm-menu-wrap"} style={this.getStyles('menuWrap')}>
             {styles.svg ? (
               <div className="bm-morph-shape" style={this.getStyles('morphShape')}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 800" preserveAspectRatio="none">
+                <svg width="100%" height="100%" viewBox="0 0 100 800" preserveAspectRatio="none">
                   <path d={styles.svg.pathInitial}/>
                 </svg>
               </div>
