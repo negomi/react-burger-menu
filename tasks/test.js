514983cbe9b863c67347c0b00ed5b4d2b18e9fbe
Imogen <imogen.wentworth@gmail.com>
2016-07-04 14:19:51 -0700

    Revert "Force tests to exit on single run"
    This reverts commit c15591ea68f9ed2e8efbd7e8a446b14a05f0eeb7.

--- tasks/test.js
+++ tasks/test.js
@@ -5,17 +5,11 @@ require('../test/utils/dom.js');
 module.exports = function(gulp, config) {
   gulp.task('test', ['build:lib'], function() {
     var reporter = process.argv[process.argv.indexOf('--reporter') + 1];
-    var runOnce = process.argv.indexOf('--run-once') > -1;
 
     return gulp.src(config.paths.test, {read: false})
       .pipe(mocha({reporter: reporter}))
       .on('error', function(err) {
         this.emit('end');
-      })
-      .once('end', function() {
-        if (runOnce) {
-          process.exit();
-        }
       });
   });
 
