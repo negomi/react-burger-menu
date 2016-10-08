87d7f78f8226fca873a8b0fc9e07406a956690d0
Imogen <imogen.wentworth@gmail.com>
2016-10-08 16:46:14 -0700

    Update 2016-10-08T23:46:11.635Z

--- bundle.js
+++ bundle.js
@@ -297,8 +297,6 @@ exports['default'] = function (styles) {
             }
         },
         handleExternalWrapper: function handleExternalWrapper(id, wrapperStyles, set) {
-            var html = document.querySelector('html');
-            var body = document.querySelector('body');
             var wrapper = document.getElementById(id);
             if (!wrapper) {
                 console.error('Element with ID \'' + id + '\' not found');
@@ -310,12 +308,6 @@ exports['default'] = function (styles) {
                     wrapper.style[prop] = set ? builtStyles[prop] : '';
                 }
             }
-            [
-                html,
-                body
-            ].forEach(function (element) {
-                element.style['overflow-x'] = set ? 'hidden' : '';
-            });
         },
         getStyles: function getStyles(el, index) {
             var propName = 'bm' + el.replace(el.charAt(0), el.charAt(0).toUpperCase());
