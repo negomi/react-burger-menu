975e012b01fbe68e020d7fb07beb1e2b2237aa0c
Imogen <imogen.wentworth@gmail.com>
2017-02-25 09:31:00 -0800

    Tabs -> spaces

--- src/baseStyles.js
+++ src/baseStyles.js
@@ -2,59 +2,59 @@
 
 let styles = {
 
-	overlay(isOpen) {
-		return {
-			position: 'fixed',
-			zIndex: 1,
-			width: '100%',
-			height: '100%',
-			background: 'rgba(0, 0, 0, 0.3)',
-			opacity: isOpen ? 1 : 0,
-			transform: isOpen ? '' : 'translate3d(100%, 0, 0)',
-			MozTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
-			MsTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
-			OTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
-			WebkitTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
-			transition: isOpen ? 'opacity 0.3s' : 'opacity 0.3s, transform 0s 0.3s'
-		};
-	},
+  overlay(isOpen) {
+    return {
+      position: 'fixed',
+      zIndex: 1,
+      width: '100%',
+      height: '100%',
+      background: 'rgba(0, 0, 0, 0.3)',
+      opacity: isOpen ? 1 : 0,
+      transform: isOpen ? '' : 'translate3d(100%, 0, 0)',
+      MozTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
+      MsTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
+      OTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
+      WebkitTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
+      transition: isOpen ? 'opacity 0.3s' : 'opacity 0.3s, transform 0s 0.3s'
+    };
+  },
 
-	menuWrap(isOpen, width, right) {
-		return {
-			position: 'fixed',
-			right: right ? 0 : 'inherit',
-			zIndex: 2,
-			width,
-			height: '100%',
-			transform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
-			MozTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
-			MsTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
-			OTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
-			WebkitTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
-			transition: 'all 0.5s'
-		};
-	},
+  menuWrap(isOpen, width, right) {
+    return {
+      position: 'fixed',
+      right: right ? 0 : 'inherit',
+      zIndex: 2,
+      width,
+      height: '100%',
+      transform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      MozTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      MsTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      OTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      WebkitTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      transition: 'all 0.5s'
+    };
+  },
 
-	menu() {
-		return {
-			height: '100%',
-			boxSizing: 'border-box',
-			overflow: 'auto'
-		};
-	},
+  menu() {
+    return {
+      height: '100%',
+      boxSizing: 'border-box',
+      overflow: 'auto'
+    };
+  },
 
-	itemList() {
-		return {
-			height: '100%'
-		};
-	},
+  itemList() {
+    return {
+      height: '100%'
+    };
+  },
 
-	item() {
-		return {
-			display: 'block',
-			outline: 'none'
-		};
-	}
+  item() {
+    return {
+      display: 'block',
+      outline: 'none'
+    };
+  }
 
 };
 
