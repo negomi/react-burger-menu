22b592a5e966533eabe5aa094a1a194c2e17f6d4
Imogen <imogen.wentworth@gmail.com>
2018-05-31 14:19:14 -0700

    v2.5.0

--- dist/react-burger-menu.js
+++ dist/react-burger-menu.js
@@ -1702,6 +1702,7 @@ exports['default'] = function (styles) {
                             if (item) {
                                 var extraProps = {
                                         key: index,
+                                        className: 'bm-item ' + _this3.props.itemClassName,
                                         style: _this3.getStyles('item', index, item.props.style)
                                     };
                                 return _react2['default'].cloneElement(item, extraProps);
@@ -1750,6 +1751,7 @@ exports['default'] = function (styles) {
         ]),
         id: _propTypes2['default'].string,
         isOpen: _propTypes2['default'].bool,
+        itemClassName: _propTypes2['default'].string,
         itemListClassName: _propTypes2['default'].string,
         menuClassName: _propTypes2['default'].string,
         morphShapeClassName: _propTypes2['default'].string,
@@ -1774,6 +1776,7 @@ exports['default'] = function (styles) {
         crossClassName: '',
         disableCloseOnEsc: false,
         id: '',
+        itemClassName: '',
         itemListClassName: '',
         menuClassName: '',
         morphShapeClassName: '',
