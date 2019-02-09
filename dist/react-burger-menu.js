5c89005d60cdf16e0ed53fe3fc091014214a8eb2
Imogen <imogen.wentworth@gmail.com>
2019-02-08 19:39:54 -0800

    v2.6.4

--- dist/react-burger-menu.js
+++ dist/react-burger-menu.js
@@ -1415,7 +1415,11 @@ exports['default'] = function (styles) {
                                     firstItem.focus();
                                 }
                             } else {
-                                document.activeElement.blur();
+                                if (document.activeElement) {
+                                    document.activeElement.blur();
+                                } else {
+                                    document.body.blur();
+                                }
                             }
                             _this.timeoutId && clearTimeout(_this.timeoutId);
                             _this.timeoutId = setTimeout(function () {
